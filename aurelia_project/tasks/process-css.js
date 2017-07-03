a25b84036c5667b53b11508b096b947054a69138
leiavoia <leiavoia@users.noreply.github.com>
2017-07-02 20:04:19 -0700

    cleanup for re-nesting aurelia project

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -1,19 +1,12 @@
 import gulp from 'gulp';
-import changedInPlace from 'gulp-changed-in-place';
 import sourcemaps from 'gulp-sourcemaps';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
+import sass from 'gulp-sass';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
 
 export default function processCSS() {
-  let processors = [
-    autoprefixer({browsers: ['last 1 version']})
-  ];
-
   return gulp.src(project.cssProcessor.source)
-    .pipe(changedInPlace({firstPass:true}))
     .pipe(sourcemaps.init())
-    .pipe(postcss(processors))
+    .pipe(sass().on('error', sass.logError))
     .pipe(build.bundle());
-};
+}
