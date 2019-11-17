8dd415c949e8188b1f425ab7e97c34e8a6b4eeae
leiavoia <leiavoia@users.noreply.github.com>
2019-11-16 17:17:54 -0800

    Added tech tags and icons.

--- src/classes/Tech.js
+++ src/classes/Tech.js
@@ -72,6 +72,7 @@ export let Techs = {
 	FLEETSPEED1: {
 		name: "Fleet Warp Bubble",
 		desc: '<b>+50 Ship Speed</b>. Makes all ships go faster.',
+		tags: ['propulsion'],
 		onComplete( civ ) { 
 			civ.mods.Add( new Mod('speed', '+', 50, this.name) );
 			civ.fleets.forEach( f => f.ReevaluateStats() );
@@ -80,6 +81,7 @@ export let Techs = {
 	FLEETSPEED2: {
 		name: "Fleet Warp Booster",
 		desc: '<b>+100 Ship Speed</b>. Makes all ships go even faster.',
+		tags: ['propulsion'],
 		onComplete( civ ) { 
 			civ.mods.Add( new Mod('speed', '+', 100, this.name) );
 			civ.fleets.forEach( f => f.ReevaluateStats() );
@@ -88,6 +90,7 @@ export let Techs = {
 	FLEETSPEED3: {
 		name: "Fleet HyperWarp",
 		desc: '<b>+200 Ship Speed</b>. Makes all ships go screaming fast.',
+		tags: ['propulsion'],
 		onComplete( civ ) { 
 			civ.mods.Add( new Mod('speed', '+', 200, this.name) );
 			civ.fleets.forEach( f => f.ReevaluateStats() );
@@ -96,6 +99,7 @@ export let Techs = {
 	FLEETSPEED4: {
 		name: "Fleet MegaWarp",
 		desc: '<b>+350 Ship Speed</b>. Makes all ships go like a flying mammal out of the underworld.',
+		tags: ['propulsion'],
 		onComplete( civ ) { 
 			civ.mods.Add( new Mod('speed', '+', 350, this.name) );
 			civ.fleets.forEach( f => f.ReevaluateStats() );
@@ -105,6 +109,7 @@ export let Techs = {
 	TROOPS1: {
 		name: "Basic Infantry",
 		desc: 'Our first invasion-ready ground forces.',
+		tags: ['weapons'],
 		onComplete( civ ) { 
 			let bp = new GroundUnitBlueprint();
 			bp.name = 'Basic Infantry';
@@ -120,6 +125,7 @@ export let Techs = {
 	TROOPS2: {
 		name: "Advanced Infantry",
 		desc: 'Improved invasion forces handily take on the opposition.',
+		tags: ['weapons'],
 		onComplete( civ ) { 
 			let bp = new GroundUnitBlueprint();
 			bp.name = 'Advanced Infantry';
@@ -135,6 +141,7 @@ export let Techs = {
 	TROOPS3: {
 		name: "Battleoids",
 		desc: 'Massive hybrid war machines capable of handling entire battles by themselves.',
+		tags: ['weapons'],
 		onComplete( civ ) { 
 			let bp = new GroundUnitBlueprint();
 			bp.name = 'Battleoids';
@@ -150,6 +157,7 @@ export let Techs = {
 	TROOPS4: {
 		name: "Droid Army",
 		desc: 'An overwhelming flood of robotic militants to do your dirty work.',
+		tags: ['weapons'],
 		onComplete( civ ) { 
 			let bp = new GroundUnitBlueprint();
 			bp.name = 'Droid Army';
@@ -166,121 +174,144 @@ export let Techs = {
 	HULL1: {
 		name: "Starship Engineering 1",
 		desc: '<b>Allows B-Class starships</b>.',
+		tags: ['engineering'],
 		onComplete( civ ) { civ.max_hull_size = 100; }
 		},
 	HULL2: {
 		name: "Starship Engineering 2",
 		desc: '<b>Allows C-Class starships</b>.',
+		tags: ['engineering'],
 		onComplete( civ ) { civ.max_hull_size = 200; }
 		},
 	HULL3: {
 		name: "Starship Engineering 3",
 		desc: '<b>Allows D-Class starships</b>.',
+		tags: ['engineering'],
 		onComplete( civ ) { civ.max_hull_size = 400; }
 		},
 	HULL4: {
 		name: "Starship Engineering 4",
 		desc: '<b>Allows E-Class starships</b>.',
+		tags: ['engineering'],
 		onComplete( civ ) { civ.max_hull_size = 800; }
 		},
 	HULL5: {
 		name: "Starship Engineering 5",
 		desc: '<b>Allows F-Class starships</b>.',
+		tags: ['engineering'],
 		onComplete( civ ) { civ.max_hull_size = 1200; }
 		},
 	HULL6: {
 		name: "Starship Engineering 6",
 		desc: '<b>Allows G-Class starships</b>.',
+		tags: ['engineering'],
 		onComplete( civ ) { civ.max_hull_size = 2000; }
 		},
 	HULL7: {
 		name: "Starship Engineering 7",
 		desc: '<b>Allows H-Class starships</b>.',
+		tags: ['engineering'],
 		onComplete( civ ) { civ.max_hull_size = 3000; }
 		},
 		
 	VIS1: {
 		name: "Hyperspace Observation",
-		desc: '<b>Allows us to view hyperspace objects on the map and see predictive paths of enemy fleets.</b>.',
+		desc: 'Allows us to view hyperspace objects on the map and see predictive paths of enemy fleets.',
+		tags: ['physics'],
 		onComplete( civ ) { civ.vis_level = Math.max( 1, civ.vis_level ); }
 		},
 	VIS2: {
 		name: "Subspace Observation",
-		desc: '<b>Allows us to view subspace objects on the map and see predictive paths of enemy fleets.</b>.',
+		desc: 'Allows us to view subspace objects on the map and see predictive paths of enemy fleets.',
+		tags: ['physics'],
 		onComplete( civ ) { civ.vis_level = Math.max( 1, civ.vis_level ); }
 		},
 		
 	SHIPRANGE1: {
 		name: "Improved Warp Drive",
 		desc: '<b>1000 Ship Range</b>. Basic FTL, or "warp drive", got us into deep space. Now improvements on this basic system will allows us to explore further into space.',
+		tags: ['propulsion'],
 		onComplete( civ ) { civ.ship_range = 1000; civ.RecalcEmpireBox(); }
 		},
 	SHIPRANGE2: {
 		name: "Advanced Warp Drive",
 		desc: '<b>1250 Ship Range</b>. Further refinements in warp drive technology have yielded this pinnacle achievement. This is likely as far as we can take warp technology without rethinking FTL entirely.',
+		tags: ['propulsion'],
 		onComplete( civ ) { civ.ship_range = 1250; civ.RecalcEmpireBox(); }
 		},
 	SHIPRANGE3: {
 		name: "Hyperdrive",
 		desc: '<b>1750 Ship Range</b>. Hyperdrives work on entirely different principal than Warp Drives, using low areas in hypersapce topology to quickly navigate through normal space.',
+		tags: ['propulsion'],
 		onComplete( civ ) { civ.ship_range = 1750; civ.RecalcEmpireBox(); }
 		},
 	SHIPRANGE4: {
 		name: "Turbo Hyperdrive",
 		desc: '<b>2500 Ship Range</b>. This ultra-advanced Hyperdrive uses predictive hyperspace pathfinding to squeeze every bit of efficiency out of hyperspace travel. In a nutshell, it\'s wicked fast.',
+		tags: ['propulsion'],
 		onComplete( civ ) { civ.ship_range = 2500; civ.RecalcEmpireBox(); }
 		},
 	SHIPRANGE5: {
 		name: "Ultimate Drive",
 		desc: '<b>3500 Ship Range</b>. It just works.',
+		tags: ['propulsion'],
 		onComplete( civ ) { civ.ship_range = 3500; civ.RecalcEmpireBox(); }
 		},
 		
 	HABITATION1: {
 		name: "Improved Habitation",
 		desc: '<b>+1 Habitation</b>. Living on alien planets can be difficult and expensive. These new habitation techniques will allow us to colonize even more hostile planets. With further refinements, we can live just about anywhere.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.habitation += 1; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 	HABITATION2: {
 		name: "Advanced Habitation",
 		desc: '<b>+1 Habitation</b>. Breakthroughs in engineering and materials science has given us the ability to settle on particularly ugly planets. They are ugly now, but someday they will be wonderful.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.habitation += 1; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 	HABITATION3: {
 		name: "Superior Habitation",
 		desc: '<b>+1 Habitation</b>. Living on alient planets is easy with new pre-fabricated, self-assembling shelters. It\'s still a dismal life, but it\'s one less colony our neighbors will get.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.habitation += 1; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 	HABITATION4: {
 		name: "Ultimate Habitation",
 		desc: '<b>+5 Habitation</b>. Ultimate xeno-engineering technologies mean we can live virtually anywhere in the galaxy.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.habitation += 5; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 		
 	ADAPTATION1: {
 		name: "Basic Terraforming",
 		desc: '<b>+1 Adaptation</b>.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.adaptation += 1; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 	ADAPTATION2: {
 		name: "Advanced Terraforming",
 		desc: '<b>+1 Adaptation</b>.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.adaptation += 1; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 	ADAPTATION3: {
 		name: "Superior Terraforming",
 		desc: '<b>+1 Adaptation</b>.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.adaptation += 1; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 	ADAPTATION4: {
 		name: "Ultimate Terraforming",
 		desc: '<b>+2 Adaptation</b>.',
+		tags: ['biology'],
 		onComplete( civ ) { civ.race.env.adaptation += 2; } /* TODO: hibitation tech should not affect the civ's race directly */
 		},
 		
 	XENOCOMM1: {
 		name: "Xeno Communication Skills",
 		desc: '<b>+1 Communication</b>.',
+		tags: ['socialogy'],
 		onComplete( civ ) { 
 			civ.diplo.skill += 0.1; 
 			for ( let [contact,acct] of civ.diplo.contacts ) {
@@ -293,6 +324,7 @@ export let Techs = {
 	XENOCOMM2: {
 		name: "Xeno Linguistic Mastery",
 		desc: '<b>+1 Communication</b>.',
+		tags: ['socialogy'],
 		onComplete( civ ) { 
 			civ.diplo.skill += 0.1; 
 			for ( let [contact,acct] of civ.diplo.contacts ) {
@@ -305,6 +337,7 @@ export let Techs = {
 	XENOCOMM3: {
 		name: "Alien Negotiation Skills",
 		desc: '<b>+1 Communication</b>.',
+		tags: ['socialogy'],
 		onComplete( civ ) { 
 			civ.diplo.skill += 0.1; 
 			for ( let [contact,acct] of civ.diplo.contacts ) {
@@ -322,6 +355,7 @@ for ( let k of Object.keys(WeaponList) ) {
 	Techs[k] = {
 		name: WeaponList[k].name,
 		desc: WeaponList[k].desc,
+		tags: ['weapons'],
 		onComplete( civ ) { civ.avail_ship_weapons.push( WeaponList[k] ); }
 		};
 	}
@@ -331,6 +365,7 @@ for ( let k of Object.keys(ShipComponentList) ) {
 	Techs[k] = {
 		name: ShipComponentList[k].name,
 		desc: ShipComponentList[k].desc,
+		tags: ['engineering'], // TODO differentiate
 		onComplete( civ ) { civ.avail_ship_comps.push( ShipComponentList[k] ); }
 		};
 	}
@@ -340,6 +375,7 @@ for ( let k of Object.keys(ZoneList) ) {
 	Techs['ZONE_' + k] = {
 		name: ZoneList[k].name,
 		desc: ZoneList[k].desc,
+		tags: [ZoneList[k].type],
 		onComplete( civ ) { civ.AddAvailZoneType(ZoneList[k]); }
 		};
 	}
@@ -671,6 +707,7 @@ for ( let k in Techs ) {
 	Techs[k].icon = Techs[k].icon || 'img/workshop/icons/star.png';
 	Techs[k].name = Techs[k].name || 'UNKNOWN';	
 	Techs[k].desc = Techs[k].desc || 'Missing Description';	
+	Techs[k].tags = Techs[k].tags || [];	
 	}
 	
 for ( let k in TechNodes ) { 
@@ -679,6 +716,7 @@ for ( let k in TechNodes ) {
 	TechNodes[k].icon = TechNodes[k].icon || 'img/workshop/icons/star.png';
 	TechNodes[k].rp = TechNodes[k].rp || 0;	
 	TechNodes[k].requires = TechNodes[k].requires || [];
+	TechNodes[k].tags = TechNodes[k].tags || [];
 	// if there is no yield, look for a similarly named tech (usually the same anyway)
 	if ( !TechNodes[k].yields && Techs.hasOwnProperty(k) ) {
 		TechNodes[k].yields = [k];	
@@ -691,4 +729,9 @@ for ( let k in TechNodes ) {
 	if ( !TechNodes[k].desc ) {
 		TechNodes[k].desc = Techs[ TechNodes[k].yields[0] ].desc;	
 		}
+	// merge tags from all provided techs
+	for ( let y of TechNodes[k].yields ) { 
+		TechNodes[k].tags.push( ...Techs[y].tags );
+		}
+	TechNodes[k].tags = TechNodes[k].tags.unique();
 	}
