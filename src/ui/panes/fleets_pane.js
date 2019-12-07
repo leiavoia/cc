9f6aa041fe861f1b8c60e82ae895b7f599e22120
leiavoia <leiavoia@users.noreply.github.com>
2019-12-06 21:38:09 -0800

    Table sorting improvements.

--- src/ui/panes/fleets_pane.js
+++ src/ui/panes/fleets_pane.js
@@ -2,6 +2,7 @@ export class FleetsPane {
 
 	activate(data) {
 		this.app = data.app;
+		this.fleets = this.app.game.myciv.fleets;//.map( x => x );
 		}
 		
 	 ClosePanel() {
@@ -14,4 +15,27 @@ export class FleetsPane {
 		this.app.FocusMap(o);	
 		}
 		
+	SortBy( sort_by ) {
+		this.sort_by = sort_by || this.sort_by;
+		this.sort_dir = this.sort_by == sort_by ? (this.sort_dir=='asc' ? 'desc' : 'asc') : 'desc';
+		// generic sorting method
+		let val = x => { 
+			switch ( this.sort_by ) {
+				case 'id': return x.id;
+				case 'ships': return x.ships.length;
+				case 'star': return (x.star ? x.star.name : '');
+				case 'dest': return (x.dest ? x.dest.name : '');
+				case 'milval': return x.milval || 0;
+				case 'fp': return x.fp || 0;
+				case 'health': return (x.health / x.healthmax);
+				case 'troops': return x.troops || 0;
+				case 'research': return x.research || 0;
+				}	
+			}
+		this.fleets.sort( (a,b) => val(b) < val(a) ? 1 : (val(b) > val(a) ? -1 : 0) );
+		if ( this.sort_dir == 'desc' ) {
+			this.fleets.reverse();
+			}
+		}
+				
 	}
--- src/ui/panes/planets_pane.js
+++ src/ui/panes/planets_pane.js
@@ -43,25 +43,25 @@ export class PlanetsPane {
 		}
 		
 	SortBy( sort_by ) {
-		this.sort_dir = this.sort_by == sort_by ? (this.sort_dir=='desc' ? 'asc' : 'desc') : 'asc';
 		this.sort_by = sort_by || this.sort_by;
+		this.sort_dir = this.sort_by == sort_by ? (this.sort_dir=='asc' ? 'desc' : 'asc') : 'desc';
 		// generic sorting method
 		let val = x => { 
 			switch ( this.sort_by ) {
 				case 'name': return x.name;
 				case 'size': return x.size;
-				case 'hab': return x.Adaptation(this.app.game.myciv.race);
+				case 'hab': return x.Adaptation(this.app.game.myciv.race) || 0;
 				case 'energy': return x.energy;
-				case 'pop': return x.total_pop;
-				case 'popmax': return x.maxpop;
-				case 'taxrate': return x.tax_rate;
-				case 'spending': return x.spending;
-				case 'income': return x.acct_total;
+				case 'pop': return x.total_pop || 0;
+				case 'popmax': return x.maxpop || 0;
+				case 'taxrate': return x.tax_rate || 0;
+				case 'spending': return x.spending || 0;
+				case 'income': return x.acct_total.$ || 0;
 				case 'owner': return x.owner ? x.owner.name : null;
 				case 'resources': return Object.values(x.resources).reduce( (accum,current) => accum+current, 0 );
 				}	
 			}
-		this.planets.sort( (a,b) => val(b) > val(a) ? 1 : (val(b) < val(a) ? -1 : 0) );
+		this.planets.sort( (a,b) => val(b) < val(a) ? 1 : (val(b) > val(a) ? -1 : 0) );
 		if ( this.sort_dir == 'desc' ) {
 			this.planets.reverse();
 			}
